e168f8b3e2fff6f1a90121b6355528d709bfd06c
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2025-01-23 20:45:02 +0900

    eslint: import

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 
 if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(
-    <React.StrictMode>
+  createRoot(rootElement).render(
+    <StrictMode>
       <App />
-    </React.StrictMode>,
+    </StrictMode>,
   );
 }
 
