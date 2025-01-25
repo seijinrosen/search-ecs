a3b48c35d583f2f854b0e0fe179c0f1e5a378a89
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2025-01-26 03:17:03 +0900

    refactor: replace template literals with SEARCH_QUERY constant in ecSites

--- src/ec-sites.tsx
+++ src/ec-sites.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable no-template-curly-in-string */
-
 import GoogleIcon from "@mui/icons-material/Google";
 import { Typography } from "@mui/material";
 import { DiYahooSmall } from "react-icons/di";
@@ -14,46 +12,49 @@ interface ECSiteType {
   name: string;
 }
 
+// eslint-disable-next-line no-template-curly-in-string
+export const SEARCH_QUERY = "${searchQuery}";
+
 export const ecSites: ECSiteType[] = [
   {
-    baseUrl: "https://www.amazon.co.jp/s?k=${searchQuery}",
+    baseUrl: `https://www.amazon.co.jp/s?k=${SEARCH_QUERY}`,
     icon: <FaAmazon />,
     id: 0,
     name: "Amazon.co.jp",
   },
   {
-    baseUrl: "https://search.rakuten.co.jp/search/mall/${searchQuery}/",
+    baseUrl: `https://search.rakuten.co.jp/search/mall/${SEARCH_QUERY}/`,
     icon: <SiRakuten />,
     id: 1,
     name: "楽天市場",
   },
   {
-    baseUrl: "https://www.yodobashi.com/?word=${searchQuery}",
+    baseUrl: `https://www.yodobashi.com/?word=${SEARCH_QUERY}`,
     icon: <Typography variant="h5">ヨ</Typography>,
     id: 2,
     name: "ヨドバシ.com",
   },
   {
-    baseUrl: "https://www.google.com/search?q=${searchQuery}",
+    baseUrl: `https://www.google.com/search?q=${SEARCH_QUERY}`,
     icon: <GoogleIcon />,
     id: 3,
     name: "Google",
   },
   {
-    baseUrl: "https://shopping.yahoo.co.jp/search?p=${searchQuery}",
+    baseUrl: `https://shopping.yahoo.co.jp/search?p=${SEARCH_QUERY}`,
     icon: <DiYahooSmall />,
     id: 4,
     name: "Yahoo!ショッピング",
   },
   {
-    baseUrl: "https://kakaku.com/search_results/${searchQuery}/",
+    baseUrl: `https://kakaku.com/search_results/${SEARCH_QUERY}/`,
     encode: "SJIS",
     icon: <Typography variant="h5">価</Typography>,
     id: 5,
     name: "価格.com",
   },
   {
-    baseUrl: "https://jp.mercari.com/search?keyword=${searchQuery}",
+    baseUrl: `https://jp.mercari.com/search?keyword=${SEARCH_QUERY}`,
     icon: <Typography variant="h5">メ</Typography>,
     id: 6,
     name: "メルカリ",
