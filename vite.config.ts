51fc63a698b423dc5f04f9ad1fa24607ca7a10f1
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2025-05-07 05:22:43 +0900

    feat: ビルド時の日時をlog

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    __BUILD_DATE__: JSON.stringify(new Date().toISOString()),
+  },
   plugins: [react()],
 });
