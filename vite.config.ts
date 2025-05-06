d9199a4c158b6ae28fec4a6b3e30f6baea3cb44b
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2025-05-07 05:41:18 +0900

    fix: JSTでのビルド日時を生成する関数を追加し、__BUILD_DATE__を更新

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+// JSTでのビルド日時を生成
+function getJstIsoString() {
+  const date = new Date();
+  const jstDate = new Date(date.getTime() + 9 * 60 * 60 * 1000); // UTC+9
+  return jstDate.toISOString().replace("T", " ").replace("Z", " +09:00");
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    __BUILD_DATE__: JSON.stringify(new Date().toISOString()),
+    __BUILD_DATE__: JSON.stringify(getJstIsoString()),
   },
   plugins: [react()],
 });
